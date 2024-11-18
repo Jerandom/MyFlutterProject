d2483c2640d3b3377c7a064bfb6d8575a7a3c852
Jerandom <89083263+Jerandom@users.noreply.github.com>
2024-11-18 11:33:50 +0800

    setup for google sign in

--- my_flutter_project/lib/Class/Firebase/firebaseApi.dart
+++ my_flutter_project/lib/Class/Firebase/firebaseApi.dart
@@ -1,14 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class FirebaseApi {
   Future<User?> signInWithEmailPassword(String email, String password) async {
     try {
+      // Trigger email and password sign-in
       final UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
       return userCredential.user;
     } on FirebaseAuthException catch (e) {
+      // Firebase sign in failed
       if (e.code == 'user-not-found') {
         print('No user found for that email.');
       } else if (e.code == 'wrong-password') {
@@ -18,6 +21,36 @@ class FirebaseApi {
       }
     } catch (e) {
       print('An unknown error occurred: $e');
+    }
+
+    return null;
+  }
+
+  Future<User?> signInWithGoogle() async {
+    try {
+      // Trigger the Google Sign-In flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      if (googleUser == null) {
+        // User canceled the sign-in
+        return null;
+      }
+
+      // Obtain the Google Sign-In authentication details
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+      // Create a new credential using the authentication details
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      // Sign in with the credential in Firebase
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+
+      return userCredential.user;
+    } catch (e) {
+      print("Google Sign-In Error: $e");
     }
 
     return null;
