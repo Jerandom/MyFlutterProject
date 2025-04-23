ab7c0c5a0c113cb6ede4561d40503644f8470108
Jerandom <89083263+Jerandom@users.noreply.github.com>
2025-04-23 17:53:50 +0800

    added onbaording

---
+++ my_flutter_project/lib/Class/Providers/onboardingProvider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Define a state notifier for managing the app state
+class OnBoardingProvider extends StateNotifier<ImageState> {
+  OnBoardingProvider() : super(ImageState.initial()) {
+    //constructor
+  }
+
+  
+}
+
+// Define a provider for Image Provider
+final onboardingProvider = StateNotifierProvider<OnBoardingProvider, ImageState>((ref) {
+  return OnBoardingProvider();
+});
---
+++ my_flutter_project/lib/Pages/onboarding/onboardingPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class MyLoginPage extends ConsumerWidget {
+  const MyLoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+  }
+}
