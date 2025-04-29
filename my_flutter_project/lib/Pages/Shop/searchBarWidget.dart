cb83f7c67ea10d729a2076f94ac35249e0ac69a1
Jerandom <89083263+Jerandom@users.noreply.github.com>
2025-04-29 14:03:23 +0800

    added search bar

---
+++ my_flutter_project/lib/Pages/Shop/searchBarWidget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MyLoginPage extends ConsumerWidget {
+  const MyLoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+        Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+
+            ),
+        ),
+    ),
+  }
+}
