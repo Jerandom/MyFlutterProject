0e076842d8df73cb2a9f2de96eb2514ec2e636a7
Jerandom <89083263+Jerandom@users.noreply.github.com>
2025-04-29 14:21:02 +0800

    search bar

--- my_flutter_project/lib/Pages/Shop/searchBarWidget.dart
+++ my_flutter_project/lib/Pages/Shop/searchBarWidget.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MyLoginPage extends ConsumerWidget {
-  const MyLoginPage({super.key});
+class SearchBarWidget extends ConsumerWidget {
+  const SearchBarWidget({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -11,7 +11,20 @@ class MyLoginPage extends ConsumerWidget {
         Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Container(
-
+                width: 20,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    color: Theme.of(context)
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Theme.of(context))
+                ),
+                child: Row(
+                    children: [
+                        const Icon(Iconsax.search_normal, color: Theme.of(context)),
+                        const SizedBox(width: 20),
+                        Text('Search', style: Theme.of(context).textTheme.bodySmall),
+                    ],
+                ),
             ),
         ),
     ),
